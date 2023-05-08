9da6c0ea5cbe029b375cb0d3276d8c63a3a2a106
blau <serzh.plichko@mail.ru>
2023-05-08 17:02:07 +1100

    finished class Channel

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,23 @@
+import json
+import os
+from googleapiclient.discovery import build
+
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = os.getenv('API_KEY')
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()['items']
+        self.channel_info = channel_data
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        print(self.channel_info)
+
+
+
+
+
