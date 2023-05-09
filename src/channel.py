2bae5cbbe6a35abb4fcdff73a8bcdd9c11084f50
blau <serzh.plichko@mail.ru>
2023-05-09 15:07:52 +1100

    hw 2

--- src/channel.py
+++ src/channel.py
@@ -3,19 +3,54 @@ import os
 from googleapiclient.discovery import build
 
 
-
 class Channel:
-    """Класс для ютуб-канала"""
     api_key = os.getenv('API_KEY')
 
-    def __init__(self, channel_id: str) -> None:
-        self.channel_id = channel_id
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
-        channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()['items']
-        self.channel_info = channel_data
+    @classmethod
+    def get_service(cls):
+        youtube = build('youtube', 'v3', developerKey=cls.api_key)
+        return youtube
 
-    def print_info(self) -> None:
-        print(self.channel_info)
+    def __init__(self, channel_id):
+        youtube = self.get_service()
+        request = youtube.channels().list(part='snippet,statistics', id=channel_id)
+        response = request.execute()
+
+        if response.get('items'):
+            channel_info = response['items'][0]
+            self.id_channel = channel_info['id']
+            self.title = channel_info['snippet']['title']
+            self.description = channel_info['snippet']['description']
+            self.url = f"https://www.youtube.com/channel/{channel_id}"
+            self.subscriber_count = channel_info['statistics']['subscriberCount']
+            self.video_count = channel_info['statistics']['videoCount']
+            self.view_count = channel_info['statistics']['viewCount']
+        else:
+            raise Exception(f"Не найден канал по данному id: {channel_id}")
+
+    def print_info(self):
+        print(f"id канала: {self.id_channel}")
+        print(f"название канала: {self.title}")
+        print(f"описание канала: {self.description}")
+        print(f"ссылка на канал: {self.url}")
+        print(f"количество подписчиков: {self.subscriber_count}")
+        print(f"количество видео: {self.video_count}")
+        print(f"общее количество просмотров: {self.view_count}")
+
+    def to_json(self, filename):
+        data = {
+            'channel_id': self.id_channel,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count
+        }
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+
+
 
 
 
