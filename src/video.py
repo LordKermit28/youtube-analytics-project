3d338efd1b5bc812ae9a23923e38c85b3be3c0e2
blau <serzh.plichko@mail.ru>
2023-05-19 14:34:00 +1100

    hw 4

---
+++ src/video.py
@@ -0,0 +1,54 @@
+import os
+from googleapiclient.discovery import build
+
+API_KEY = os.getenv('API_KEY')
+
+class Video:
+
+    def __init__(self, video_id: str):
+        self.video_id = video_id
+
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        request = youtube.videos().list(
+            part="snippet",
+            id=self.video_id
+        )
+        response = request.execute()
+        self.title = response['items'][0]['snippet']['title']
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo:
+    def __init__(self, id_video, id_playlist):
+        self.id_video = id_video
+        self.id_playlist = id_playlist
+        self.title = self._get_title()
+        self.link = f"https://www.youtube.com/watch?v={id_video}"
+        self.views_count = self._get_video_info('viewCount')
+        self.likes_count = self._get_video_info('likeCount')
+
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def _get_title(self):
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        request = youtube.videos().list(
+            part='snippet',
+            id=self.id_video
+        )
+        response = request.execute()
+        return response['items'][0]['snippet']['title']
+
+    def _get_video_info(self, data_type: str):
+        youtube = build('youtube', 'v3', developerKey=API_KEY)
+        request = youtube.videos().list(
+            part='statistics',
+            id=self.id_video
+        )
+        response = request.execute()
+        return int(response['items'][0]['statistics'][data_type])
+
+
