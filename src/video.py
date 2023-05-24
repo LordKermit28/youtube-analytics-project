05c019981ac2b4297326035aa09b82c6e78eab9b
blau <serzh.plichko@mail.ru>
2023-05-24 18:54:13 +1100

    hw 4

--- src/video.py
+++ src/video.py
@@ -1,4 +1,6 @@
 import os
+
+
 from googleapiclient.discovery import build
 
 API_KEY = os.getenv('API_KEY')
@@ -7,6 +9,10 @@ class Video:
 
     def __init__(self, video_id: str):
         self.video_id = video_id
+        self.title = self._get_title()
+        self.link = f"https://www.youtube.com/watch?v={video_id}"
+        self.views_count = self._get_video_info('viewCount')
+        self.likes_count = self._get_video_info('likeCount')
 
         youtube = build('youtube', 'v3', developerKey=API_KEY)
         request = youtube.videos().list(
@@ -19,25 +25,11 @@ class Video:
     def __str__(self):
         return self.title
 
-
-class PLVideo:
-    def __init__(self, id_video, id_playlist):
-        self.id_video = id_video
-        self.id_playlist = id_playlist
-        self.title = self._get_title()
-        self.link = f"https://www.youtube.com/watch?v={id_video}"
-        self.views_count = self._get_video_info('viewCount')
-        self.likes_count = self._get_video_info('likeCount')
-
-
-    def __str__(self):
-        return f'{self.title}'
-
     def _get_title(self):
         youtube = build('youtube', 'v3', developerKey=API_KEY)
         request = youtube.videos().list(
             part='snippet',
-            id=self.id_video
+            id=self.video_id
         )
         response = request.execute()
         return response['items'][0]['snippet']['title']
@@ -46,9 +38,26 @@ class PLVideo:
         youtube = build('youtube', 'v3', developerKey=API_KEY)
         request = youtube.videos().list(
             part='statistics',
-            id=self.id_video
+            id=self.video_id
         )
         response = request.execute()
         return int(response['items'][0]['statistics'][data_type])
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, id_playlist):
+        super().__init__(video_id)
+        self.id_playlist = id_playlist
+        self.title = self._get_title()
+        self.link = f"https://www.youtube.com/watch?v={video_id}"
+        self.views_count = self._get_video_info('viewCount')
+        self.likes_count = self._get_video_info('likeCount')
+
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+
 
 
